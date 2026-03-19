c65dba8e65b153e01eb3091ee835a37fda65d668
GPIO65535 <3524521262@qq.com>
2026-03-19 18:32:56 +0800

    butterfly

---
+++ Core/Src/app_freertos.c
@@ -0,0 +1,219 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * File Name          : app_freertos.c
+  * Description        : Code for freertos applications
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "FreeRTOS.h"
+#include "task.h"
+#include "main.h"
+#include "cmsis_os.h"
+
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+#include"tim.h"
+#include"adc.h"
+#include<math.h>
+
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN PTD */
+
+/* USER CODE END PTD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+
+#define Servo_Min_angle 60.0f
+#define Servo_Max_angle 120.0f
+#define Servo_Step 5.0f
+TaskHandle_t Servo_Handle;
+#define Servo_Task_priority 5
+#define Servo_Task_Stack_Size 128
+TaskHandle_t Battery_Handle;
+#define Battery_Task_priority 4
+#define Battery_Task_Stack_Size 128
+TaskHandle_t LED_Handle;
+#define LED_Task_priority 3
+#define LED_Task_Stack_Size 128
+TaskHandle_t Control_Handle;
+#define Control_Task_priority 6
+#define Control_Task_Stack_Size 128
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN Variables */
+extern volatile uint16_t ppm_recv[];
+extern volatile uint8_t ppm_flag;
+/* USER CODE END Variables */
+osThreadId defaultTaskHandle;
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN FunctionPrototypes */
+void Servo_Task(void *para);
+void Battery_Task(void *para);
+void LED_Task(void *para);
+void Control_Task(void *para);
+/* USER CODE END FunctionPrototypes */
+
+void StartDefaultTask(void const * argument);
+
+void MX_FREERTOS_Init(void); /* (MISRA C 2004 rule 8.1) */
+
+/**
+  * @brief  FreeRTOS initialization
+  * @param  None
+  * @retval None
+  */
+void MX_FREERTOS_Init(void) {
+  /* USER CODE BEGIN Init */
+  
+  /* USER CODE END Init */
+
+  /* USER CODE BEGIN RTOS_MUTEX */
+  /* add mutexes, ... */
+  /* USER CODE END RTOS_MUTEX */
+
+  /* USER CODE BEGIN RTOS_SEMAPHORES */
+  /* add semaphores, ... */
+  /* USER CODE END RTOS_SEMAPHORES */
+
+  /* USER CODE BEGIN RTOS_TIMERS */
+  /* start timers, add new ones, ... */
+  /* USER CODE END RTOS_TIMERS */
+
+  /* USER CODE BEGIN RTOS_QUEUES */
+  /* add queues, ... */
+  /* USER CODE END RTOS_QUEUES */
+
+  /* Create the thread(s) */
+  /* definition and creation of defaultTask */
+  osThreadDef(defaultTask, StartDefaultTask, osPriorityNormal, 0, 128);
+  defaultTaskHandle = osThreadCreate(osThread(defaultTask), NULL);
+
+  /* USER CODE BEGIN RTOS_THREADS */
+  xTaskCreate(Servo_Task, "Servo_Task", Servo_Task_Stack_Size, NULL, Servo_Task_priority, &Servo_Handle);
+  xTaskCreate(Battery_Task, "Battery_Task", Battery_Task_Stack_Size, NULL, Battery_Task_priority, &Battery_Handle);
+  xTaskCreate(LED_Task, "LED_Task", LED_Task_Stack_Size, NULL, LED_Task_priority, &LED_Handle);
+  xTaskCreate(Control_Task, "Control_Task", Control_Task_Stack_Size, NULL, Control_Task_priority, &Control_Handle);
+  /* add threads, ... */
+  /* USER CODE END RTOS_THREADS */
+
+}
+
+/* USER CODE BEGIN Header_StartDefaultTask */
+/**
+  * @brief  Function implementing the defaultTask thread.
+  * @param  argument: Not used
+  * @retval None
+  */
+/* USER CODE END Header_StartDefaultTask */
+void StartDefaultTask(void const * argument)
+{
+  /* USER CODE BEGIN StartDefaultTask */
+  /* Infinite loop */
+  for(;;)
+  {
+    osDelay(1);
+  }
+  /* USER CODE END StartDefaultTask */
+}
+
+/* Private application code --------------------------------------------------*/
+/* USER CODE BEGIN Application */
+volatile float ctrl_bias = 0.0f;  /* ×ªÏòÆ«²î: Ò£¿ØÆ÷CH1, ¡À15¡ã */
+
+static uint32_t AngleToPulse(float angle)
+{
+  return (uint32_t)((angle / 180.0f) * 2000.0f + 500.0f);
+}
+void Servo_Task(void *para)
+{
+  TickType_t xLastWakeTime = xTaskGetTickCount();
+  float center = (Servo_Max_angle + Servo_Min_angle) / 2.0f;
+  float amplitude = (Servo_Max_angle - Servo_Min_angle) / 2.0f;
+  float phase = 0.0f;
+  float phase_step = 2.0f * 3.14159265f / ((Servo_Max_angle - Servo_Min_angle) / Servo_Step * 2.0f);
+
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_1);
+  HAL_TIM_PWM_Start(&htim1, TIM_CHANNEL_2);
+  while(1)
+  {
+    float angle = center + amplitude * sinf(phase);
+    float bias = ctrl_bias;
+    uint32_t pulse1=AngleToPulse(angle + bias);
+    uint32_t pulse2=AngleToPulse(180.0f - angle - bias);
+    __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_1,pulse1);
+    __HAL_TIM_SET_COMPARE(&htim1,TIM_CHANNEL_2,pulse2);
+    phase += phase_step;
+    if(phase >= 2.0f * 3.14159265f)
+      phase -= 2.0f * 3.14159265f;
+    vTaskDelayUntil(&xLastWakeTime,pdMS_TO_TICKS(10));
+  }
+}
+volatile float voltage;
+void Battery_Task(void*para)
+{
+  static volatile uint16_t adc_value[1];
+  HAL_ADCEx_Calibration_Start(&hadc1, ADC_SINGLE_ENDED);
+  HAL_ADC_Start_DMA(&hadc1,(uint32_t *)adc_value,1);
+  while(1)
+  {
+    voltage=adc_value[0]*3.3f/4095;
+    vTaskDelay(pdMS_TO_TICKS(500));
+  }
+}
+void LED_Task(void*para)
+{
+  while(1)
+  {
+    if(voltage<3.0f)
+    {
+      HAL_GPIO_WritePin(Charge_LED_GPIO_Port,Charge_LED_Pin,GPIO_PIN_SET);
+      vTaskDelay(pdMS_TO_TICKS(500));
+      HAL_GPIO_WritePin(Charge_LED_GPIO_Port,Charge_LED_Pin,GPIO_PIN_RESET);
+      vTaskDelay(pdMS_TO_TICKS(500));
+    }
+    else
+    {
+      HAL_GPIO_WritePin(Charge_LED_GPIO_Port,Charge_LED_Pin,GPIO_PIN_SET);
+      vTaskDelay(pdMS_TO_TICKS(200));
+    }
+  }
+}
+void Control_Task(void*para)
+{
+  HAL_TIM_IC_Start_IT(&htim2,TIM_CHANNEL_4);
+  while(1)
+  {
+    if(ppm_flag)
+    {
+      ppm_flag=0;
+      /* CH1 ×óÓÒÒ¡¸Ë ¡ú ×ªÏò: 1500us¾ÓÖÐ, ¡À500us ¡ú ¡À15¡ã */
+      ctrl_bias = (ppm_recv[0] - 1500) / 500.0f * 15.0f;
+    }
+    vTaskDelay(pdMS_TO_TICKS(20));
+  }
+}
+/* USER CODE END Application */
+
---
+++ Core/Src/stm32g4xx_it.c
@@ -0,0 +1,257 @@
+/* USER CODE BEGIN Header */
+/**
+  ******************************************************************************
+  * @file    stm32g4xx_it.c
+  * @brief   Interrupt Service Routines.
+  ******************************************************************************
+  * @attention
+  *
+  * Copyright (c) 2026 STMicroelectronics.
+  * All rights reserved.
+  *
+  * This software is licensed under terms that can be found in the LICENSE file
+  * in the root directory of this software component.
+  * If no LICENSE file comes with this software, it is provided AS-IS.
+  *
+  ******************************************************************************
+  */
+/* USER CODE END Header */
+
+/* Includes ------------------------------------------------------------------*/
+#include "main.h"
+#include "stm32g4xx_it.h"
+/* Private includes ----------------------------------------------------------*/
+/* USER CODE BEGIN Includes */
+/* USER CODE END Includes */
+
+/* Private typedef -----------------------------------------------------------*/
+/* USER CODE BEGIN TD */
+
+/* USER CODE END TD */
+
+/* Private define ------------------------------------------------------------*/
+/* USER CODE BEGIN PD */
+#define PPM_CHANNELS 8
+volatile uint16_t ppm_recv[PPM_CHANNELS];
+volatile uint16_t ppm_buf[PPM_CHANNELS];
+volatile uint8_t ppm_flag;
+static volatile uint32_t ppm_last_capture;
+static volatile uint16_t ppm_channel_indx;
+/* USER CODE END PD */
+
+/* Private macro -------------------------------------------------------------*/
+/* USER CODE BEGIN PM */
+
+/* USER CODE END PM */
+
+/* Private variables ---------------------------------------------------------*/
+/* USER CODE BEGIN PV */
+
+/* USER CODE END PV */
+
+/* Private function prototypes -----------------------------------------------*/
+/* USER CODE BEGIN PFP */
+
+/* USER CODE END PFP */
+
+/* Private user code ---------------------------------------------------------*/
+/* USER CODE BEGIN 0 */
+
+/* USER CODE END 0 */
+
+/* External variables --------------------------------------------------------*/
+extern DMA_HandleTypeDef hdma_adc1;
+extern TIM_HandleTypeDef htim1;
+extern TIM_HandleTypeDef htim2;
+extern TIM_HandleTypeDef htim15;
+
+/* USER CODE BEGIN EV */
+
+/* USER CODE END EV */
+
+/******************************************************************************/
+/*           Cortex-M4 Processor Interruption and Exception Handlers          */
+/******************************************************************************/
+/**
+  * @brief This function handles Non maskable interrupt.
+  */
+void NMI_Handler(void)
+{
+  /* USER CODE BEGIN NonMaskableInt_IRQn 0 */
+
+  /* USER CODE END NonMaskableInt_IRQn 0 */
+  /* USER CODE BEGIN NonMaskableInt_IRQn 1 */
+   while (1)
+  {
+  }
+  /* USER CODE END NonMaskableInt_IRQn 1 */
+}
+
+/**
+  * @brief This function handles Hard fault interrupt.
+  */
+void HardFault_Handler(void)
+{
+  /* USER CODE BEGIN HardFault_IRQn 0 */
+
+  /* USER CODE END HardFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_HardFault_IRQn 0 */
+    /* USER CODE END W1_HardFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Memory management fault.
+  */
+void MemManage_Handler(void)
+{
+  /* USER CODE BEGIN MemoryManagement_IRQn 0 */
+
+  /* USER CODE END MemoryManagement_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_MemoryManagement_IRQn 0 */
+    /* USER CODE END W1_MemoryManagement_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Prefetch fault, memory access fault.
+  */
+void BusFault_Handler(void)
+{
+  /* USER CODE BEGIN BusFault_IRQn 0 */
+
+  /* USER CODE END BusFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_BusFault_IRQn 0 */
+    /* USER CODE END W1_BusFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Undefined instruction or illegal state.
+  */
+void UsageFault_Handler(void)
+{
+  /* USER CODE BEGIN UsageFault_IRQn 0 */
+
+  /* USER CODE END UsageFault_IRQn 0 */
+  while (1)
+  {
+    /* USER CODE BEGIN W1_UsageFault_IRQn 0 */
+    /* USER CODE END W1_UsageFault_IRQn 0 */
+  }
+}
+
+/**
+  * @brief This function handles Debug monitor.
+  */
+void DebugMon_Handler(void)
+{
+  /* USER CODE BEGIN DebugMonitor_IRQn 0 */
+
+  /* USER CODE END DebugMonitor_IRQn 0 */
+  /* USER CODE BEGIN DebugMonitor_IRQn 1 */
+
+  /* USER CODE END DebugMonitor_IRQn 1 */
+}
+
+/******************************************************************************/
+/* STM32G4xx Peripheral Interrupt Handlers                                    */
+/* Add here the Interrupt Handlers for the used peripherals.                  */
+/* For the available peripheral interrupt handler names,                      */
+/* please refer to the startup file (startup_stm32g4xx.s).                    */
+/******************************************************************************/
+
+/**
+  * @brief This function handles DMA1 channel1 global interrupt.
+  */
+void DMA1_Channel1_IRQHandler(void)
+{
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 0 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 0 */
+  HAL_DMA_IRQHandler(&hdma_adc1);
+  /* USER CODE BEGIN DMA1_Channel1_IRQn 1 */
+
+  /* USER CODE END DMA1_Channel1_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM1 break interrupt and TIM15 global interrupt.
+  */
+void TIM1_BRK_TIM15_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM1_BRK_TIM15_IRQn 0 */
+
+  /* USER CODE END TIM1_BRK_TIM15_IRQn 0 */
+  if (htim1.Instance != NULL)
+  {
+    HAL_TIM_IRQHandler(&htim1);
+  }
+  if (htim15.Instance != NULL)
+  {
+    HAL_TIM_IRQHandler(&htim15);
+  }
+  /* USER CODE BEGIN TIM1_BRK_TIM15_IRQn 1 */
+
+  /* USER CODE END TIM1_BRK_TIM15_IRQn 1 */
+}
+
+/**
+  * @brief This function handles TIM2 global interrupt.
+  */
+void TIM2_IRQHandler(void)
+{
+  /* USER CODE BEGIN TIM2_IRQn 0 */
+
+  /* USER CODE END TIM2_IRQn 0 */
+  HAL_TIM_IRQHandler(&htim2);
+  /* USER CODE BEGIN TIM2_IRQn 1 */
+
+  /* USER CODE END TIM2_IRQn 1 */
+}
+
+/* USER CODE BEGIN 1 */
+void HAL_TIM_IC_CaptureCallback(TIM_HandleTypeDef *htim)
+{
+  if(htim->Instance==TIM2 && htim->Channel==HAL_TIM_ACTIVE_CHANNEL_4)
+  {
+    uint32_t capture=HAL_TIM_ReadCapturedValue(&htim2, TIM_CHANNEL_4);
+    uint32_t diff;
+    if(capture>=ppm_last_capture)
+    {
+      diff=capture-ppm_last_capture;
+    }
+    else
+    {
+      diff=(65536+capture)-ppm_last_capture;
+    }
+    ppm_last_capture=capture;
+    if(diff>2500)
+    {
+      if(ppm_channel_indx==PPM_CHANNELS)
+      {
+        for(uint8_t i=0;i<PPM_CHANNELS;i++)
+        {
+          ppm_recv[i]=ppm_buf[i];
+        }
+        ppm_flag=1;
+      }
+      ppm_channel_indx=0;
+    }
+    else if(diff>=800 && diff<=2200)
+    {
+      if(ppm_channel_indx<PPM_CHANNELS)
+      {
+        ppm_buf[ppm_channel_indx]=(uint16_t)diff;
+        ppm_channel_indx++;
+      }
+    }
+  }
+}
+/* USER CODE END 1 */
